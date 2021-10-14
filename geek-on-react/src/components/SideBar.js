21a3f1da7bc599f18ad38dccfb10f8165282dbe3
cesar villa <cvillaf@hotmail.com>
2021-10-13 21:40:20 -0700

    .

--- geek-on-react/src/components/SideBar.js
+++ geek-on-react/src/components/SideBar.js
@@ -32,7 +32,7 @@ function SideBar(){
 
                 {/*<!-- Nav Item - Pages -->*/}
                 <li className="nav-item">
-                    <a className="nav-link collapsed" href="https://geek-on-front/productos/crear/">
+                    <a className="nav-link collapsed" href="https://geek-on-front.herokuapp.com/productos/crear/">
                         <i className="fas fa-fw fa-folder"></i>
                         <span>Crear Producto</span>
                     </a>                    
